5eab30c24ccf92f974423d11b3b99af5924ef556
aubhree <aubhree@gmail.com>
2015-11-13 09:38:10 -1000

    revision 1
    Added cycle time to main.  Fixed bug so temp is a float.

--- Driver1.java
+++ Driver1.java
@@ -11,22 +11,45 @@ public class Driver1{
 	
 	public static void main(String[] args){
 		
-		int zip = 96814;
-		String data;
-		data = ((new WeatherRetriever()).retrieveData(zip));
-		Weather weather = new Weather();
-		WeatherParser weatherObj = new WeatherParser();
+		final int zip = 96814; //Sets the zip code to check temp
+		final int cycle = 30000; //Controls the lenght of time the temp will refresh
+		String data; //Data gathered from webpage stored in here
+		WeatherRetriever wr = new WeatherRetriever(); //Object that opens webpage and retrieves JSON data
+		WeatherParser weatherObj = new WeatherParser(); //Object to parse JSON data
+		Weather weather = new Weather(); //Object to hold temperature and humidity data
 		
-		try {
+		while(true){
 			
-			weather = weatherObj.getWeather(data);	
+			data = wr.retrieveData(zip);
+			try {
+				
+				//Gathers weather data and parses it
+				weather = weatherObj.getWeather(data);	
 			
-		} catch (JSONException e){
-			System.err.println ("Invalid entry");
+				//Prints to screen the weather
+				System.out.println("Today's temperature is: " + weather.getTemp() + 
+				" Degrees Fahrenheit @ zip: " + zip);
+			
+			} catch (JSONException e){
+			
+				System.err.println ("Invalid entry");
+			
+			}
+		
+			try {
+			
+				Thread.sleep(cycle);           //Sleeps program for x amount of miliseconds
+			
+			} catch(InterruptedException ex) {
+			
+				Thread.currentThread().interrupt();
+			
+			}
+			
+			
 		}
 		
-		System.out.println("Today's temperature is: " + weather.getTemp() + " Degrees Fahrenheit");
-	
+		
 	}
 
 }
